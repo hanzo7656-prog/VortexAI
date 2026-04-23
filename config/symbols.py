0752611d7d5daefc70552bb1d02774a95b99bdcc
hanzo7656-prog <hanzo7656@gmail.com>
2026-04-23 17:27:13 +0330

    Create symbols.py

---
+++ config/symbols.py
@@ -0,0 +1,26 @@
+# 20 نماد با حجم معاملاتی بالا (بر اساس مستندات نوبیتکس)
+HIGH_VOLUME_SYMBOLS = [
+    "BTCIRT",   # بیت‌کوین ریال
+    "ETHIRT",   # اتریوم ریال
+    "USDTIRT",  # تتر ریال
+    "DOGEIRT",  # دوج ریال
+    "XRPIRT",   # ریپل ریال
+    "BNBIRT",   # بایننس ریال
+    "TRXIRT",   # ترون ریال
+    "ADAIRT",   # کاردانو ریال
+    "MATICIRT", # پالیگان ریال
+    "SOLIRT",   # سولانا ریال
+    "LTCIRT",   # لایت‌کوین ریال
+    "DOTIRT",   # پولکادات ریال
+    "AVAXIRT",  # آوالانچ ریال
+    "SHIBIRT",  # شیبا ریال
+    "LINKIRT",  # چین لینک ریال
+    "ATOMIRT",  # کازماس ریال
+    "UNIIRT",   # یونی سوآپ ریال
+    "FTMIRT",   # فانتوم ریال
+    "NEARIRT",  # نیر پروتکل ریال
+    "APTIRT",   # آپتوس ریال
+]
+
+# برای توسعه، با 5 نماد شروع کنید
+DEV_SYMBOLS = ["BTCIRT", "ETHIRT", "USDTIRT", "DOGEIRT", "XRPIRT"]
